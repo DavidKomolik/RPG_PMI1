eff0bfcccbe7ee3e0e672d65f60dad675067b2f7
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-10-09 17:15:37 +0200

    unity project finished

---
+++ Cvicenie1/Cvicenie1/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float sensitivity = 0.2f;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 moveDirection = new Vector3(0,0,1);
+            transform.Translate(moveDirection * sensitivity);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 moveDirection = new Vector3(0, 0, -1);
+            transform.Translate(moveDirection * sensitivity);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 moveDirection = new Vector3(-1, 0, 0);
+            transform.Translate(moveDirection * sensitivity);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 moveDirection = new Vector3(1, 0, 0);
+            transform.Translate(moveDirection * sensitivity);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            Vector3 moveDirection = new Vector3(0, 1, 0);
+            transform.Rotate(moveDirection);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Vector3 moveDirection = new Vector3(0, -1, 0);
+            transform.Rotate(moveDirection);
+        }
+    }
+}

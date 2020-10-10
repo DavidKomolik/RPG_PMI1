c2afd9aa8ed018514d668b659ddbb31ebda524aa
DavidKomolik <38225017+DavidKomolik@users.noreply.github.com>
2020-10-10 22:11:18 +0200

    upravena dokumentacia

--- Cvicenie1/Cvicenie1/Assets/Scripts/Player.cs
+++ Cvicenie1/Cvicenie1/Assets/Scripts/Player.cs
@@ -11,8 +11,8 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            Vector3 moveDirection = new Vector3(0,0,1);
-            transform.Translate(moveDirection * sensitivity);
+            Vector3 moveDirection = new Vector3(0,0,1); // smer pohybu
+            transform.Translate(moveDirection * sensitivity); // zmena transformácie (pozície) objektu hráča vzhľadom na smer pohybu
         }
         if (Input.GetKey(KeyCode.S))
         {
